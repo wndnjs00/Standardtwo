d0bc0f8c52ee8e2b4ac59e83510aca9ff1ca0ff1
jeonjuwon <juwonjeon852@gmail.com>
2024-03-28 17:31:22 +0900

    Initial commit

--- app/src/main/java/com/example/standardtwo/DonutListFragment.kt
+++ app/src/main/java/com/example/standardtwo/DonutListFragment.kt
@@ -5,24 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.standardtwo.databinding.FragmentDonutListBinding
 
 class DonutListFragment : Fragment() {
 
-    lateinit var binding : FragmentDonutListBinding
+    lateinit var binding: FragmentDonutListBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        return inflater.inflate(R.layout.fragment_donut_list, container, false)
         binding = FragmentDonutListBinding.inflate(inflater, container, false)
-
-
-        binding.textView.setOnClickListener {
-            
-        }
 
 
         return binding.root
