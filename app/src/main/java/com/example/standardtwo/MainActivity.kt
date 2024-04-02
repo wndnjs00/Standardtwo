71888a52581611659c1bf805fe72625646794f7a
jeonjuwon <juwonjeon852@gmail.com>
2024-04-02 16:31:48 +0900

    features/pr브랜치 push

--- app/src/main/java/com/example/standardtwo/MainActivity.kt
+++ app/src/main/java/com/example/standardtwo/MainActivity.kt
@@ -6,24 +6,11 @@ import com.example.standardtwo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 뷰바인딩 사용(초기화해줌)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
-        // 버튼 클릭시
-        binding.button.setOnClickListener {
-            // frameLayout에 DonutListFragment연결
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.frameLayout, DonutListFragment())
-                .commit()
-        }
 
 
     }
